0769d96fc3dffae56ea549a1ddf95e3cacd7fd16
Nicholas Araújo <njaf@ic.ufal.br>
2020-09-17 16:41:10 -0300

    Added game over condition and game over alert

--- script.js
+++ script.js
@@ -60,6 +60,14 @@ function initGame() {
   if (snake[0].y < 0 * box && direction == "up") {
     snake[0].y = 16 * box;
   }
+
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      clearInterval(gameLoop);
+      alert("Game over!");
+    }
+  }
+
   createBG();
   createSnake();
   createFood();
